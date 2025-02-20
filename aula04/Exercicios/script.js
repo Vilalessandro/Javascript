83174b241f14fca6e22e4bcedd11d9d688f9f110
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-02-20 19:22:20 +0100

    update exercise

--- aula04/Exercicios/script.js
+++ aula04/Exercicios/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar (){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 19
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minuto(s).`
+    if (hora >= 0 && hora <= 12 ){
+        //BOM DIA
+        img.src = 'manha.jpg'
+        document.body.style.background = '#ACBABF'
+    } else if (hora >= 13 && hora <= 18){
+        //BOA TARDE
+        img.src = 'tarde.jpg'
+        document.body.style.background ='#e9b2949f'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.jpg'
+        document.body.style.background =' #636363b4'
+    }
+}

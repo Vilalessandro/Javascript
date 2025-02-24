053048774137c0421e0828bcfd3e851bb177bd8a
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-02-24 19:26:35 +0100

    Update ex016.js

--- aula04/ex016.js
+++ aula04/ex016.js
@@ -1,10 +1,15 @@
 var c = 1
-while (c <= 6) {
-    console.log('Tudo bem?');
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 6)
+
+/*var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`);
     c++
 }
 
-/*
 console.log('Tudo bem')
 console.log('Tudo bem')
 console.log('Tudo bem')

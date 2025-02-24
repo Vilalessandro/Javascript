fb42c43cf7e97a396920d37f6f9f5164ef3e24aa
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-02-24 18:49:36 +0100

    update

--- aula04/Exercicios/Verificador de idade/script.js
+++ aula04/Exercicios/Verificador de idade/script.js
@@ -3,45 +3,49 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano){
+    if (fano.value.length == '0' ||  Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else{
+    } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        /*var img = document.createElement('img')
-        img.setAttibute('id', 'foto')*/
-        if (fsex[0].checked){
+        var img = document.createElement('img')
+        img.style.width = '250px'
+        img.style.height = '250px'
+        img.style.paddingTop = '5px'
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
             genero = 'Homem'
-            /*if (idade >= 0 && idade < 10){
+            if (idade >= 0 && idade < 10){
                 //Criança
-                img.setAttibute('src', 'meninoGimp.png')
+                img.setAttribute('src', 'meninoGimp.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemHomem.png')
             } else if (idade < 50){
                 //Adulto
-                img.setAttibute('src', 'homemGimp.png')
+                img.setAttribute('src', 'homemGimp.png')
             } else {
                 //Idoso
-                img.setAttibute('src', 'idosoGimp.png')
-            }*/
-        } else (fsex[1].checked);{
+                img.setAttribute('src', 'idosoGimp.png')
+            }
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
-            /*if (idade >= 0 && idade < 10){
+            if (idade >= 0 && idade < 10){
                 //Criança
-                img.setAttibute('src', 'meninaGimp.png')
+                img.setAttribute('src', 'meninaGimp.png')
             } else if (idade < 21) {
                 //Jovem
-                //img.setAttibute('src', 'meninaGimp.png')
+                img.setAttribute('src', 'jovemMulher.png')
             } else if (idade < 50){
                 //Adulto
-                img.setAttibute('src', 'mulherGimp.png')
+                img.setAttribute('src', 'mulherGimp.png')
             } else {
                 //Idoso
-                img.setAttibute('src', 'idosaGimp.png')
-            }*/
+                img.setAttribute('src', 'idosaGimp.png')
+            }
         }
         res.innerHTML = `Dectamos um(a) ${genero} com ${idade} anos.`
-        //res.appendChild(img)
+        res.appendChild(img)
     }
 }

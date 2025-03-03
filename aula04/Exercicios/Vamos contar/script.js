f3274e17a94d5da8ee45119879407f57b39ee238
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-03-03 19:24:05 +0100

    update

--- aula04/Exercicios/Vamos contar/script.js
+++ aula04/Exercicios/Vamos contar/script.js
@@ -4,16 +4,30 @@ function contar(){
     let passo = document.getElementById('txtp') 
     let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = '<strong>Impossivel contar, falta alguma coisa \u{1FAE4}</strong>'
+        //window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = '<strong>Contando:</strong> <br> '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        
+        if (p <= 0){
+            window.alert('Passo inválido. Começando do PASSO 1')
+            p = 1
         }
+        /* c recebe i(inicio) Enquanto o c, for menor ou igual o f(fim), o c soma e recebe o p(passo)  */
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p)  {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }

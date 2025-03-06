ca9a807f6f4f7264d5b41ea31b2d7f282c086e8c
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-03-06 19:01:45 +0100

    create exercises functions

---
+++ Aula16/function02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0)/*O numero 0 depois do n1, serve para quando tiver só um numero na soma, ele não retornar undefined, retorna somente o numero qu existe*/{
+    return n1 + n2
+}
+
+console.log(soma(5, 8))

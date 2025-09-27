638d37790451b76dfa0e7c7d10513e54c4d27eb0
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-09-27 10:25:52 +0200

    Update index.js

--- Java Udemy/index.js
+++ Java Udemy/index.js
@@ -1,3 +1,11 @@
-let valor = 10
-
+var valor = 10
 console.log(valor);
+
+const c = 'Alessandro';
+console.log(c);
+
+if (valor == 11) {
+  console.log("valor tem o numero 10");  
+} else{
+    console.log("Valor não tem 10");    
+}

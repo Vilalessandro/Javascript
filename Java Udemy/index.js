19d8f702bd5f4949e8ad82e402a5a5d0401dbd4b
Alessandro Vila <126591593+Vilalessandro@users.noreply.github.com>
2025-10-01 18:53:53 +0200

    Update index.js

--- Java Udemy/index.js
+++ Java Udemy/index.js
@@ -9,3 +9,23 @@ if (valor == 11) {
 } else{
     console.log("Valor não tem 10");    
 }
+
+let y = 11.999;
+let x = 20;
+console.log(parseInt(y) + valor + x); //parseInt converte o VALOR QUEBRADO em INTEIRO.
+
+/*
+  Adição +
+  Subtração -
+  Multiplicação *
+  Divisão /
+  Resto %
+
+  typeof = para saber o tipo de variavel.
+
+  exponencial = 10e2
+*/
+
+let a = 10, b = 20, d = 2, result = (a + b) * d;
+console.log(result);
+
